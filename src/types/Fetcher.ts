405c8558555595baedf36f104eb497f42261dd75
Qrymy <regist@qry.mx>
2020-12-11 17:12:22 +0900

    Remove type Fetcher'

--- src/types/Fetcher.ts
+++
@@ -1,6 +0,0 @@
-import { CollectionPath, DocumentPath, Query } from '@/types/Firestore'
-import { AnyRecord } from '@/types/AnyRecord'
-
-export type Fetcher<T extends AnyRecord> =
-  | ((path: DocumentPath) => Promise<T | null>)
-  | ((path: CollectionPath, query?: Query<T>) => Promise<T[]>)
