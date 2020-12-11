fae87c6e29d3f9a90c5cf2793a7ce9f9a5ef3831
Qrymy <regist@qry.mx>
2020-12-11 10:07:44 +0900

    Create firebase types

---
+++ src/types/Firestore.ts
@@ -0,0 +1,42 @@
+import firebase from 'firebase/app'
+
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+type AnyRecord = Record<string, any>
+
+type DocumentSnapshot<T> = firebase.firestore.DocumentSnapshot<T>
+
+type ComparisonOperator = firebase.firestore.WhereFilterOp
+
+type OrderByDirection = firebase.firestore.OrderByDirection
+
+const collections = {
+  dump: 'dump',
+  test: 'test',
+} as const
+
+type Collection = keyof typeof collections
+
+export type CollectionPath = `/${Collection}`
+
+export type DocumentPath = `${CollectionPath}/${string}`
+
+type WhereQuery<T extends AnyRecord, K extends keyof T> = [
+  K,
+  ComparisonOperator,
+  T[K]
+]
+
+type Order<T extends AnyRecord> = [keyof T, OrderByDirection]
+
+type FieldValue<T extends AnyRecord, K extends keyof T> = T[K]
+
+export type Query<T extends AnyRecord> = {
+  readonly where?: WhereQuery<T, keyof T>[]
+  readonly order?: Order<T>[]
+  readonly limit?: number
+  readonly limitToLast?: number
+  readonly startAt?: DocumentSnapshot<T> | FieldValue<T, keyof T>[]
+  readonly startAfter?: DocumentSnapshot<T> | FieldValue<T, keyof T>[]
+  readonly endBefore?: DocumentSnapshot<T> | FieldValue<T, keyof T>[]
+  readonly endAt?: DocumentSnapshot<T> | FieldValue<T, keyof T>[]
+}
