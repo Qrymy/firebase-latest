9530bbb6f21814068ab665eeca716f9dacf49928
Qrymy <regist@qry.mx>
2020-12-11 09:36:23 +0900

    Create BrowserError type

---
+++ src/types/BrowserError.ts
@@ -0,0 +1,8 @@
+import firebase from 'firebase/app'
+
+export type BrowserError =
+  | firebase.FirebaseError
+  | firebase.auth.AuthError
+  | firebase.auth.Error
+  | firebase.storage.FirebaseStorageError
+  | firebase.firestore.FirestoreError
