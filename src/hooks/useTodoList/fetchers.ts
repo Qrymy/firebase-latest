a03a653462e878c19385bf39012eb5dcb75fbcda
Qrymy <regist@qry.mx>
2021-01-04 16:13:05 +0900

    Update hooks

--- src/hooks/useTodoList/fetchers.ts
+++ src/hooks/useTodoList/fetchers.ts
@@ -2,10 +2,13 @@ import { listDocuments } from '@/lib/firestore'
 import { Todo } from '@/types/Todo'
 import { LIMITATION } from '@/const/Limitation'
 
-export const fetcher = async (_key: string) => {
+export const fetcher = async (key: string) => {
+  const [, , cursor] = key.split('/')
+
   const todos = await listDocuments<Todo>(['todos'], {
     orderBy: [['createdAt', 'desc']],
     limit: LIMITATION.TODOS_LIST,
+    ...(cursor ? { startAfter: cursor } : {}),
   })
 
   return todos
--- src/hooks/useTodoList/useTodoList.ts
+++ src/hooks/useTodoList/useTodoList.ts
@@ -2,8 +2,18 @@ import useSWR from 'swr'
 import { fetcher } from './fetchers'
 import { Todo } from '@/types/Todo'
 
-export const useTodoList = (uid?: string, initialData?: Todo[]) => {
-  const key = uid ? '/todos' : null
+export const useTodoList = (
+  uid?: string,
+  cursorOrInitialData?: Todo['createdAt'] | Todo[]
+) => {
+  const cursor =
+    typeof cursorOrInitialData === 'number' ? cursorOrInitialData : undefined
+
+  const initialData = Array.isArray(cursorOrInitialData)
+    ? cursorOrInitialData
+    : undefined
+
+  const key = uid && cursor ? `/todos/${cursor}` : uid ? '/todos' : null
 
   const { data } = useSWR(key, fetcher, { initialData })
 
