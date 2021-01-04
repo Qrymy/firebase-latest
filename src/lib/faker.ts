b52eb8a2aa63acf19ccf6da673ee3254c1bab977
Qrymy <regist@qry.mx>
2021-01-04 13:42:14 +0900

    Update faker function

--- src/lib/faker.ts
+++ src/lib/faker.ts
@@ -1,7 +1,8 @@
 import faker from 'faker/locale/ja'
+import { Todo } from '@/types/Todo'
 
-export const createFakerTodos = (count = 1000, words = 2) => {
+export const createFakerTodos = (count = 1000, words = 2): Partial<Todo>[] => {
   return Array.from({ length: count }).map(() => {
-    return faker.lorem.words(words)
+    return { content: faker.lorem.words(words) }
   })
 }
